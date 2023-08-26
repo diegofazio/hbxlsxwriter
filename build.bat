d02aed62b26e97feb6a3cb5679f88ce89b4bbd12
DIEGO H FAZIO <52408779+diegofazio@users.noreply.github.com>
2023-08-26 20:34:04 -0300

    Add bcc64 libs

--- build.bat
+++
@@ -1,6 +0,0 @@
-SET HB_WITH_XLSXWRITER=./libxlsxwriter
-@del .\lib\x64\hbxlsxwriter.lib
-@del .\lib\x32\hbxlsxwriter.lib
-@hbmk2 hbxlsxwriter.hbp -rebuild
-@copy .\lib\x64\hbxlsxwriter.lib \harbour\lib\win\msvc64
-@copy .\lib\x32\hbxlsxwriter.lib \harbour\lib\win\msvc
